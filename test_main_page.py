a3ef70b9c5e76a7f3978b30b475f21b4c8f67650
trystep <alexeevmakc@mail.ru>
2021-03-19 11:57:46 +0300

    Вынес локаторы в отдельный файл

--- test_main_page.py
+++ test_main_page.py
@@ -4,6 +4,7 @@ from pages.main_page import MainPage
 # pytest -v --tb=line --language=en test_main_page.py
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
+    # link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer'
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
