5ae72d54706e6d0ac1cb78993bd758c7d6d73463
trystep <alexeevmakc@mail.ru>
2021-03-21 16:00:58 +0300

    Добавлен новые параметры тесты

--- test_product_page.py
+++ test_product_page.py
@@ -1,3 +1,5 @@
+import pytest
+
 from pages.base_page import BasePage
 from pages.product_page import ProductPage
 
@@ -13,9 +15,21 @@ from pages.product_page import ProductPage
 # Сообщение о том, что товар добавлен в корзину. Название товара в сообщении должно совпадать с тем товаром, который вы действительно добавили.
 # Сообщение со стоимостью корзины. Стоимость корзины совпадает с ценой товара.
 
-
-def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+@pytest.mark.parametrize('link', [
+    'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0',
+    'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1',
+    'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2',
+    'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3',
+    'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4',
+    'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5',
+    'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6',
+    'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7',
+    'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8',
+    'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9'
+])
+def test_guest_can_add_product_to_basket(browser, link):
+    # link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    # link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
